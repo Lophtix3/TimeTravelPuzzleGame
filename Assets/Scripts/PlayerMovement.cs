19f36671bcda8710f8116a6f331b105bd1f5b6af
Benjamin Sundvall <benjamin@urgott.se>
2018-11-19 21:39:25 +0100

    Added preliminary movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    
+    int i = 10;
 
     // Use this for initialization
     void Start () {
@@ -12,7 +12,21 @@ public class PlayerMovement : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        
+	void FixedUpdate () {
+        if (i == 10) {
+            if (Input.GetAxisRaw("Vertical") != 0) {
+                transform.position += new Vector3(0f, 0f, 1f) * Input.GetAxisRaw("Vertical");
+                i = 0;
+            }
+            if (Input.GetAxisRaw("Horizontal") != 0)
+            {
+                transform.position += new Vector3(1f, 0f, 0f) * Input.GetAxisRaw("Horizontal");
+                i = 0;
+            }
+        }
+        if (i < 10) {
+            i++;
+        }
+        print(i);
     }
 }
