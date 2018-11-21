4bf29617a8f72447d1738932763a55a616655e4a
Benjamin Sundvall <benjamin@urgott.se>
2018-11-21 20:17:05 +0100

    Changed player movement and materials, added test level (fixed lighting?)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,28 +4,39 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    int i = 10;
+    
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        if (i == 10) {
-            if (Input.GetAxisRaw("Vertical") != 0) {
-                transform.position += new Vector3(0f, 0f, 1f) * Input.GetAxisRaw("Vertical");
-                i = 0;
-            }
-            if (Input.GetAxisRaw("Horizontal") != 0)
-            {
-                transform.position += new Vector3(1f, 0f, 0f) * Input.GetAxisRaw("Horizontal");
-                i = 0;
-            }
+    void Start ()
+    {
+        
+    }
+
+
+
+    void LogPosition(Vector3 _pos)
+    {
+
+    }
+
+    void Move()
+    {
+        if (Input.GetButtonDown("Vertical"))
+        {
+            transform.position += new Vector3(0f, 0f, 1f) * Input.GetAxisRaw("Vertical");
         }
-        if (i < 10) {
-            i++;
+
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            transform.position += new Vector3(1f, 0f, 0f) * Input.GetAxisRaw("Horizontal");
         }
+
+        LogPosition(transform.position);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        Move();
     }
 }
