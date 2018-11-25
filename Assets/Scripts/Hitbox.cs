e3fd4a84e3c7916ec0db5dbe2e0389c4cf5519df
Jonatan Wästlund <jonte.w1@gmail.com>
2018-11-25 20:31:04 +0100

    changes

---
+++ Assets/Scripts/Hitbox.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitbox : MonoBehaviour {
+
+    public int state;
+    public int factor;
+    public Vector2 from;
+    public Vector3 to;
+    public bool teleport;
+
+    public int[] partsLayer;
+    public int playerLayer;
+
+    public GameObject[] parts;
+    public GameObject player;
+    public GameObject hitboxes;
+
+    private Vector2 direction;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay (Collider other) {
+        direction = other.gameObject.GetComponent<PlayerMovement>().Move(state);
+        if (teleport && direction == from) {
+            for (int x = 0; x<parts.Length; x++) {
+                parts[x].transform.position = new Vector3(-partsLayer[x]*factor, -partsLayer[x]*factor, partsLayer[x]*factor);
+            }
+            player.transform.position = new Vector3(-playerLayer * factor, -playerLayer * factor, playerLayer * factor) + to;
+            hitboxes.transform.position = new Vector3(-playerLayer * factor, -playerLayer * factor, playerLayer * factor);
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    
+    private float currentVertical;
+    private float currentHorizontal;
+    private float vertical = 0;
+    private float horizontal = 0;
+    private Vector2 axis;
 
     // Use this for initialization
     void Start ()
@@ -19,24 +23,42 @@ public class PlayerMovement : MonoBehaviour {
 
     }
 
-    void Move()
+    public Vector2 Move(int state)
     {
-        if (Input.GetButtonDown("Vertical"))
-        {
-            transform.position += new Vector3(0f, 0f, 1f) * Input.GetAxisRaw("Vertical");
-        }
+        vertical = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetButtonDown("Horizontal"))
-        {
-            transform.position += new Vector3(1f, 0f, 0f) * Input.GetAxisRaw("Horizontal");
-        }
+        horizontal = Input.GetAxisRaw("Horizontal");
 
-        LogPosition(transform.position);
+        if (currentHorizontal != horizontal || currentVertical != vertical) {
+            switch (state) {
+                case 1:
+                    transform.position += new Vector3(horizontal, 0f, vertical);
+                    break;
+                case 2:
+                    transform.position += new Vector3(horizontal, vertical, 0f);
+                    break;
+
+                case 3:
+                    transform.position += new Vector3(0f, vertical, horizontal);
+                    break;
+                case 4:
+                    transform.position += new Vector3(horizontal, -vertical, 0f);
+                    break;
+                case 5:
+
+                    break;
+            }
+            LogPosition(transform.position);
+            axis = new Vector2(horizontal, vertical);
+            currentVertical = vertical;
+            currentHorizontal = horizontal;
+            return axis;
+        }
+        return new Vector2(0f, 0f);
     }
 
     // Update is called once per frame
     void FixedUpdate ()
     {
-        Move();
     }
 }
